d8b4994dc1d4382d223affa791ac07409bddef82
Ayush Chauhan <38734507+ayushchauhan101@users.noreply.github.com>
2022-12-01 17:51:56 +0530

    Update transanctions.py

--- transanctions.py
+++ transanctions.py
@@ -3,9 +3,10 @@ import pandas as pd
 data = pd.read_excel('data.xlsx')
 df = pd.DataFrame(data)
 
+# find all occurences of transaction between 200 to 300
 filter_by_close = df['Close'].between(200, 300)
 
 filtered_data = df.loc[filter_by_close, ['Ticker', 'Open', 'Close']]
-print(filtered_data)
 
-# filtered_data.to_excel('new_data.xlsx')
+# to create a new excel file only with the desired data
+filtered_data.to_excel('new_data.xlsx')
